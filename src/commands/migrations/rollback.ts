4f7ef0a36c699e4279a4ef2e116270fd1d106852
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2019-12-13 21:57:59 +0200

    add rolback command

--- src/commands/migrations/rollback.ts
+++ src/commands/migrations/rollback.ts
@@ -1,5 +1,16 @@
-//NOTE: run down migration or specified one
+//Run next migration or specified one
+import { initTable, startMigration, finishMigration, deleteMigration } from './../../models/migrations'
+import { createConnection, closeConnection } from '../../mysql'
 
 export default (migrationName: string): void => {
-  console.log(`rollback to ${migrationName}`)
+  createConnection()
+
+  initTable()
+    .then(() => console.log(`rollback ${migrationName}`))
+    .then(() => deleteMigration(migrationName))
+    .then(closeConnection)
+    .catch(err => {
+      console.log(err)
+      closeConnection()
+    })
 }
