b5ade9b5232b82c00cd5748ea1da09e2b9389f24
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2019-12-13 21:06:00 +0200

    add migrations table structure

--- src/models/migrations.ts
+++ src/models/migrations.ts
@@ -1,15 +1,23 @@
 
 import { query } from './abstract-model'
+import config from '../config'
 
 type Migration = {
   id: Number,
   name: String,
-  created_at: Date,
-  updated_at: Date
+  started_at: Date,
+  finished_at: Date,
 }
 
 export const initTable = () => {
-  return query('SELECT 1;')
-
+  return query(`
+      CREATE TABLE IF NOT EXISTS ${config.migrationsDir} (
+        id INT NOT NULL AUTO_INCREMENT,
+        name CHAR(255) NOT NULL,
+        started_at DATETIME NOT NULL,
+        finished_at DATETIME NULL,
+        PRIMARY KEY (id)
+      ) ENGINE=InnoDB;
+  `)
 }
 
