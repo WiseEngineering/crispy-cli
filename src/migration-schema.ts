f0e5aa82ce7f3474af9ead17c74db658b7f966df
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2019-12-20 12:33:30 +0200

    add functionality to respond with migration should be run

--- src/migration-schema.ts
+++ src/migration-schema.ts
@@ -1,4 +1,6 @@
 import fs from 'fs'
+import { parse } from 'path'
+
 import config from './config'
 
 const { migrationsDir } = config
@@ -20,7 +22,22 @@ export const getPath = (migrationName: string): string =>
 export const isExist = (migrationName: string): boolean =>
   fs.existsSync(getPath(migrationName))
 
+/*
+Returns list of migrations should be run based on current migration version
+*/
+export const getMigrationsToRun = (currentMigrationName: string): string[] => {
+  const migrations = fs.readdirSync(migrationsDir).map(migration => parse(migration).name);
+
+  const currentMigrationIndex = migrations.findIndex(migration => migration == currentMigrationName)
+  if (!currentMigrationIndex)
+    throw new Error(`cannot find migration ${currentMigrationName} in migrations directory`)
+  // Need to increase index into 2 steps: 1 - currentMigrationIndex index from 0,
+  // the second one - we have to respond with next to current migration
+  return migrations.slice(currentMigrationIndex + 2)
+}
+
 export default {
   getPath,
-  isExist
+  isExist,
+  getMigrationsToRun
 }
