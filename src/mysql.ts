f86565677629176831057ca69a6a918582201147
svirgunvolodia <svirgun200@gmail.com>
2019-12-20 18:55:11 +0200

    move abstract model methods to mysql

--- src/mysql.ts
+++ src/mysql.ts
@@ -1,7 +1,6 @@
 import mysql, { Connection } from 'mysql'
 import config from './config'
 
-
 let mysqlConnection: Connection;
 
 export const createConnection = (): void => {
@@ -12,3 +11,16 @@ export const createConnection = (): void => {
 export const getConnection = (): Connection => mysqlConnection
 
 export const closeConnection = (): void => mysqlConnection.end()
+
+export const query = async (sql: string): Promise<Array<object>> => (
+  new Promise((resolve, reject) =>
+    getConnection().query(sql, (error, results) =>
+      error ? reject(error) : resolve(results)
+    )
+  )
+)
+
+export const esc = (value: string | number): string => getConnection().escape(value)
+
+export const formatDate = (date: Date): string =>
+  esc(date.toISOString().slice(0, 19).replace('T', ' '));
